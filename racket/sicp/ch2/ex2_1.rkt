806448fdcfb107cdf49bcdee0a0963b7d108fbb9
PaprikaZ <sms.extreme@gmail.com>
2014-11-16 16:39:44 +0800

    Add SICP excises 2.1

---
+++ racket/sicp/ch2/ex2_1.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(define [numer x] (car x))
+(define [denom x] (cdr x))
+(define [make-rat n d] (cons n d))
+
+(define [remove-gcd rat-out-prod]
+  (lambda (n d)
+    (let ([g (gcd n d)])
+      (rat-out-prod (/ n g) (/ d g)))))
+(define no-gcd-make-rat (remove-gcd make-rat))
+
+(define [move-minus rat-out-prod]
+  (lambda (n d)
+    (cond ([< d 0] (rat-out-prod (- n) (- d)))
+          (else (rat-out-prod n d)))))
+(define moved-minus-make-rat (move-minus make-rat))
+
+(define new-make-rat (move-minus (remove-gcd make-rat)))
+
+(define [add-rat x y]
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define [sub-rat x y]
+  (make-rat (- (* numer x) (denom y)
+               (* numer y) (denom x))
+            (* (denom x) (denom y))))
+
+(define [mul-rat x y]
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define [div-rat x y]
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define [print-rat x]
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(print-rat (make-rat 3 -9))
+(print-rat (no-gcd-make-rat 3 -9))
+(print-rat (moved-minus-make-rat 3 -9))
+(print-rat (new-make-rat 3 -9))
