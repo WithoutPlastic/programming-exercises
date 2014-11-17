82fda1c3a533d18b88141de4ca24917d2e38d4c5
PaprikaZ <sms.extreme@gmail.com>
2014-11-17 13:37:32 +0800

    Add SICP excises 2.6 repeat version

---
+++ racket/sicp/ch2/ex2_6_repeat.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+(define meta-f (lambda (x) (* x 2)))
+(define meta-rf (lambda (x) (/ x 2)))
+(define [repeat-twice f]
+  (lambda (x) (f (f x))))
+
+(define zero
+  (lambda (f) (lambda (rf) (lambda (x) x))))
+(define [add-one n]
+  (lambda (f) (lambda (rf) (lambda (x) (f (((n f) rf) x))))))
+(define [sub-one n]
+  (lambda (f) (lambda (rf) (lambda (x) (rf (((n f) rf) x))))))
+(define [plus n m]
+  (lambda (f) (lambda (rf) (lambda (x) (((n f) rf) (((m f) rf) x))))))
+(define [sub n m]
+  (lambda (f) (lambda (rf) (lambda (x) (((n f) rf) (((m rf) f) x))))))
+(define one (add-one zero))
+(define two (add-one (add-one zero)))
+(define three (add-one (add-one (add-one zero))))
+(define four (add-one (add-one (add-one (add-one zero)))))
+(define minus-one (sub-one zero))
+(define minus-two (sub-one (sub-one zero)))
+(define minus-three (sub-one (sub-one (sub-one zero))))
+(define minus-four (sub-one (sub-one (sub-one (sub-one zero)))))
+
+(((one meta-f) meta-rf) 1)
+((((add-one zero) meta-f) meta-rf) 1)
+
+(((two meta-f) meta-rf) 1)
+((((add-one one) meta-f) meta-rf) 1)
+((((sub-one two) meta-f) meta-rf) 1)
+
+((((plus two two) meta-f) meta-rf) 1)
+((((plus (plus two two) one) meta-f) meta-rf) 1)
+((((sub four two) meta-f) meta-rf) 1)
+
+(((((repeat-twice (repeat-twice (repeat-twice add-one))) zero)
+   meta-f) meta-rf) 1)
