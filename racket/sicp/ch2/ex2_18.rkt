f217ce0884514597aaf6c1a9bff3e8eb4d614abd
PaprikaZ <sms.extreme@gmail.com>
2014-11-17 16:17:49 +0800

    Fix ex2.18 result data type, pair to list

--- racket/sicp/ch2/ex2_18.rkt
+++ racket/sicp/ch2/ex2_18.rkt
@@ -8,6 +8,6 @@
 (define [reverse items]
   (if [null? items]
     items
-    (cons (reverse (cdr items)) (car items))))
+    (append (reverse (cdr items)) (list (car items)))))
 
 (reverse (list 1 4 9 16 25))
