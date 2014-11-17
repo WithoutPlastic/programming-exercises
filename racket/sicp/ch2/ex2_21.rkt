39ab548c57c31ae694b590d40326f82325fd38b0
PaprikaZ <sms.extreme@gmail.com>
2014-11-17 20:05:23 +0800

    Add SICP excises 2.21

---
+++ racket/sicp/ch2/ex2_21.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+;(define [square-list items]
+;  (if [null? items]
+;    items
+;    (cons
+;      (*
+;        (car items)
+;        (car items))
+;      (square-list (cdr items)))))
+
+(define [square-list items]
+  (map (lambda (x) (* x x)) items))
+
+(square-list (list 1 2 3 4))
