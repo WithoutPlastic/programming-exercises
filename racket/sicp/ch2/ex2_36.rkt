3e4e5ac64c58f4b2bd5bf535314192b4b9e3e18f
PaprikaZ <sms.extreme@gmail.com>
2014-11-18 20:16:28 +0800

    Add SICP excises 2.36

---
+++ racket/sicp/ch2/ex2_36.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define [accumulate op initial sequence]
+  (if [null? sequence]
+    initial
+    (op
+      (car sequence)
+      (accumulate op initial (cdr sequence)))))
+
+(define [accumulate-n op init seqs]
+  (if [null? (car seqs)]
+    '()
+    (cons (accumulate op init (map car seqs))
+          (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
