625fb43fcdff32b1f4b544c7ea82ff44bb17e06e
PaprikaZ <sms.extreme@gmail.com>
2014-11-19 13:50:47 +0800

    Add SICP excises 2.40

---
+++ racket/sicp/ch2/ex2_40.rkt
@@ -0,0 +1,71 @@
+#lang racket
+
+(define [accumulate op initial sequence]
+  (if [null? sequence]
+    initial
+    (op
+      (car sequence)
+      (accumulate op initial (cdr sequence)))))
+
+(define [accumulate-n op init seqs]
+  (if [null? (car seqs)]
+    '()
+    (cons (accumulate op init (map car seqs))
+          (accumulate-n op init (map cdr seqs)))))
+
+(accumulate
+  append
+  '()
+  (map
+    (lambda (i)
+      (map
+        (lambda (j) (list i j))
+        enumerate-interval 1 (- i 1)))
+    (enumerate-interval 1 n)))
+
+(define [flatmap proc seq]
+  (accumulate append '() (map proc seq)))
+
+(define [prime-sum? pair]
+  (prime? (+ (car pair) (cadr pair))))
+
+(define [make-pair-sum pair]
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define [prime-sum-pairs n]
+  (map
+    make-pair-sum
+    (filter
+      prime-sum?
+      (unique-pairs n))))
+;      (flatmap
+;        (lambda (i)
+;          (map
+;            (lambda (j) (list i j))
+;            (enumerate-interval 1 (- i 1))))
+;        (enumerate-interval 1 n)))))
+
+(define [unique-pairs n]
+  (flatmap
+    (lambda (i)
+      (map
+        (lambda (j) (list i j))
+        (enumerate-interval 1 (- i 1)))
+      (enumerate-interval 1 n))))
+
+(define [permutations s]
+  (if [null? s]
+    '()
+    (flatmap
+      (lambda (x)
+        (map
+          (lambda (p)
+            (cons x p))
+          (permutations (remove x s))))
+      s)))
+
+(define [remove item sequence]
+  (filter
+    (lambda (x)
+      (not (= x item)))
+    sequence))
