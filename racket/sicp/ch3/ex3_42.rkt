3feff100a1be2b33a13a3ad8cf03fbed2944f8fb
PaprikaZ <sms.extreme@gmail.com>
2014-12-01 17:33:21 +0800

    Add SICP exercise 3.42

---
+++ racket/sicp/ch3/ex3_42.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(define [make-serializer f] (lambda [] (f)))
+(define [parallel-execute . fs]
+  (if [null? fs]
+    'done
+    (begin
+      (car fs)
+      (parallel-execute (cdr fs)))))
+
+(define [make-account balance]
+  (define [withdraw amount]
+    (if [<= amount balance]
+      (begin
+        (set! balance (- balance amount))
+        balance)
+      "Insufficient funds"))
+  (define [deposit amount]
+    (set! balance (+ balance amount))
+    balance)
+  (let* ([protected (make-serializer)]
+         [protected-withdraw (protected withdraw)]
+         [protected-deposit (protected deposit)])
+    (define [dispatch op . args]
+      (cond ([eq? op 'withdraw] (apply protected-withdraw args))
+            ([eq? op 'deposit] (apply protected-deposit args))
+            ([eq? op 'balance] balance)
+            (else (error "unknown operation -- MAKE-ACCOUNT" op))))
+    dispatch))
+
+;Answer: seems no different
