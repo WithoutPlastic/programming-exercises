ee17bb42cffbac0320894545cceccdef3d5e3e6b
PaprikaZ <sms.extreme@gmail.com>
2015-01-09 12:38:22 +0800

    Loop until you guess right number

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -7,28 +7,30 @@ fn main() {
 
   let secret_number = (rand::random::<uint>() % 100u) + 1u;
 
-  println!("The secret number is: {}", secret_number);
+  loop {
+    println!("Please input your guess.");
+    let input = io::stdin().read_line()
+                           .ok()
+                           .expect("Failed to read line");
+    let input_num: Option<uint> = input.trim().parse();
+    let num = match input_num {
+      Some(num) => num,
+      None      => {
+        println!("Please input a number!");
+        return;
+      }
+    };
 
-  println!("Please input your guess.");
-  let input = io::stdin().read_line()
-                         .ok()
-                         .expect("Failed to read line");
+    println!("You guessed: {}", input);
 
-  let input_num: Option<uint> = input.trim().parse();
-  let num = match input_num {
-    Some(num) => num,
-    None      => {
-      println!("Please input a number!");
-      return;
+    match cmp(num, secret_number) {
+      Ordering::Less      => println!("Too small!"),
+      Ordering::Greater   => println!("Too big!"),
+      Ordering::Equal     => {
+        println!("You win!");
+        return;
+      }
     }
-  };
-
-  println!("You guessed: {}", input);
-
-  match cmp(num, secret_number) {
-    Ordering::Less      => println!("Too small!"),
-    Ordering::Greater   => println!("Too big!"),
-    Ordering::Equal     => println!("You win!"),
   }
 }
 
