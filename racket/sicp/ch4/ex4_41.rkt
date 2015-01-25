86bb3846662796d3f4888e5025a7acd59e38905d
PaprikaZ <sms.extreme@gmail.com>
2015-01-25 15:11:59 +0800

    Improve ugly ex4.41 solution

--- racket/sicp/ch4/ex4_41.rkt
+++ racket/sicp/ch4/ex4_41.rkt
@@ -6,45 +6,38 @@
         ([member (car items) (cdr items)] false)
         (else (distinct? (cdr items)))))
 
+(define [flatmap proc items]
+  (if [null? items]
+    '()
+    (let ([mapped (proc (car items))]
+          [remaining (flatmap proc (cdr items))])
+      (if [pair? mapped]
+        (append mapped remaining)
+        (cons mapped remaining)))))
+
+(define [permutations lists]
+  (if [null? lists]
+    (list '())
+    (flatmap (lambda [x] (map (lambda [y] (cons x y)) (permutations (cdr lists))))
+             (car lists))))
+
+(define [met-requirements? items]
+  (let ([b (first items)]
+        [c (second items)]
+        [f (third items)]
+        [m (fourth items)]
+        [s (fifth items)])
+    [and [not [= b 5]] [not [= c 1]] [not [= f 1]] [not [= f 5]] [< c m]
+         [not [= (abs (- s f)) 1]]
+         [not [= (abs (- f c)) 1]]
+         [distinct? items]]))
+
 (define [multi-dwelling]
   (let* ([baker '(1 2 3 4 5)]
          [cooper '(1 2 3 4 5)]
          [fletcher '(1 2 3 4 5)]
          [miller '(1 2 3 4 5)]
-         [smith '(1 2 3 4 5)]
-         [bcs (append* (map (lambda [b] (map (lambda [c] (list b c)) cooper))
-                           baker))]
-         [bcfs (append* (map (lambda [bc] (map (lambda [f] (list bc f)) fletcher))
-                            bcs))]
-         [bcfms (append* (map (lambda [bcf] (map (lambda [m] (list bcf m)) miller))
-                              bcfs))]
-         [bcfmss (append* (map (lambda [bcfm] (map (lambda [s] (list bcfm s)) smith))
-                               bcfms))]
-         [combinations (map (lambda [x] (list (caaaar x)
-                                              (cadr (caaar x))
-                                              (cadaar x)
-                                              (cadar x)
-                                              (cadr x))) bcfmss)])
-    ;(display combinations)
-    (define [iter remaining]
-      (if [null? remaining]
-        (display "DONE")
-        (let* ([combination (first remaining)]
-               [rests (rest remaining)]
-               [b (first combination)]
-               [c (second combination)]
-               [f (third combination)]
-               [m (fourth combination)]
-               [s (fifth combination)])
-          (when [and [not [= b 5]]
-                   [not [= c 1]]
-                   [not [= f 1]]
-                   [not [= f 5]]
-                   [< c m]
-                   [not [= (abs (- s f)) 1]]
-                   [not [= (abs (- f c)) 1]]
-                   [distinct? combination]]
-            (begin (newline)(newline)(display combination)))
-          (iter rests))))
-    (iter combinations)))
+         [smith '(1 2 3 4 5)])
+    (filter met-requirements?
+            (permutations (list baker cooper fletcher miller smith)))))
 (multi-dwelling)
