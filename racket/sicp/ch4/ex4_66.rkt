5aabcc7f0e315b7123bc99e375710059f34af1a5
PaprikaZ <sms.extreme@gmail.com>
2015-01-31 17:20:54 +0800

    Add SICP exercise 4.66

---
+++ racket/sicp/ch4/ex4_66.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+;Answer: When I see the title, I double checked the sum of computer programmer
+;query, it don't give any duplications. But the wheel query do. Duplications
+;make Ben's solution not work. And then, I recall the previous exercise, exclude
+;duplications in result pairs which using alphabetic. Here, some kind of
+;table/dictionary/set data structure is needed.
