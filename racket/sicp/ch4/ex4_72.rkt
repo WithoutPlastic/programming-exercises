1547669bd3c43620bc3ea7f3755298fa580b5f7c
PaprikaZ <sms.extreme@gmail.com>
2015-02-06 21:00:14 +0800

    Add SICP exercise 4.72

---
+++ racket/sicp/ch4/ex4_72.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+;Answer: I didn't go back to old exercise for answer. The interleave is simple
+;version and it is recursive. It appear to solve simulate mixing a infinite
+;stream. One side effect is that, we can see all stream information even if all
+;of them is infinite stream.
