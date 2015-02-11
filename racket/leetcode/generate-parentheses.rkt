7934f96573406be25277dc4f6122a327d3c17e3d
PaprikaZ <sms.extreme@gmail.com>
2015-02-11 15:51:48 +0800

    Add leetcode exercise generate-parentheses

---
+++ racket/leetcode/generate-parentheses.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+;Problem:
+;Given n pairs of parentheses, write a function to generate all combinations of
+;well-formed parentheses.
+;
+;For example, given n = 3, a solution set is:
+;
+;"((()))", "(()())", "(())()", "()(())", "()()()".
+
+(define op #\() (define cp #\))
+
+(define [repeat-parenthesis n]
+  (append (build-list n (const op)) (build-list n (const cp))))
+
+(define [repeat-prefix n]
+  (let ([prefix-range (range 1 n)])
+    (apply append
+           (map (lambda [r]
+                  (map (lambda [x] (append (repeat-parenthesis r) x))
+                       (generate-parenthesis (- n r))))
+                prefix-range))))
+
+(define [outter-enclosing n]
+  (let ([enclosing-repeats (range (- n 2) 0 -1)])
+    (apply append
+           (map (lambda [r]
+                  (map (lambda [x] (append (build-list r (const op))
+                                           x
+                                           (build-list r (const cp)))) 
+                       (repeat-prefix (- n r))))
+                enclosing-repeats))))
+
+(define [generate-parenthesis n]
+  (if [< 0 n]
+    (append (list (repeat-parenthesis n))
+            (repeat-prefix n)
+            (outter-enclosing n))
+    (list '())))
+
+(map list->string (generate-parenthesis 3))
+(map list->string (generate-parenthesis 4))
+;(map list->string (generate-parenthesis 5))
