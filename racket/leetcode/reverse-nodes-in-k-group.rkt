63fa9678b1a24e101d3cfa872d609bddd4d0c0fc
PaprikaZ <sms.extreme@gmail.com>
2015-02-11 19:31:46 +0800

    Add leetcode exercise reverse-nodes-in-k-group

---
+++ racket/leetcode/reverse-nodes-in-k-group.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+;Problem:
+;Given a linked list, reverse the nodes of a linked list k at a time and return
+;its modified list.
+;
+;If the number of nodes is not a multiple of k then left-out nodes in the end
+;should remain as it is.
+;
+;You may not alter the values in the nodes, only nodes itself may be changed.
+;
+;Only constant memory is allowed.
+;
+;For example,
+;Given this linked list: 1->2->3->4->5
+;
+;For k = 2, you should return: 2->1->4->3->5
+;
+;For k = 3, you should return: 3->2->1->4->5
+
+(require "linked-node.rkt")
+
+(define [create-n-node-next n] (apply compose (build-list n (const node-next))))
+(define [n-remaining? head n]
+  (if [< 0 n]
+    [and [not [last-node? head]]
+         (n-remaining? (node-next head) (sub1 n))]
+    true))
+
+(define [reverse-k-group linked-nodes k]
+  (define [reverse-section head tail]
+    (let* ([node-by-offset (lambda [offset] ((create-n-node-next offset) head))]
+           [elts (map node-by-offset (range 1 (add1 k)))]
+           [hr-elts (cons head (reverse elts))]
+           [lr-elts (reverse (cons tail elts))])
+      (for-each set-node-next! hr-elts lr-elts)))
+
+  (define [iter head]
+    (when [n-remaining? head k]
+      (reverse-section head ((create-n-node-next (add1 k)) head))
+      (iter ((create-n-node-next k) head))))
+
+  (when [< 1 k] (iter linked-nodes))
+  linked-nodes)
+
+(define test-linked-list
+  (make-linked-list (make-node 1 (make-node 2 (make-node 3
+                    (make-node 4 (make-node 5 (make-node 6
+                    (make-node 7 (make-node 8 (make-node 9
+                    (make-node 10 '()))))))))))))
+
+;(reverse-k-group test-linked-list 2)
+(reverse-k-group test-linked-list 4)
