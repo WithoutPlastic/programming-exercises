fb64a2053ade3301cebd25e856ebdba273f14fa5
PaprikaZ <sms.extreme@gmail.com>
2015-02-12 15:34:02 +0800

    Update leetcode exercise next-permutation with math solution

--- racket/leetcode/next-permutation.rkt
+++ racket/leetcode/next-permutation.rkt
@@ -15,29 +15,59 @@
 ;1,2,3 → 1,3,2
 ;3,2,1 → 1,2,3
 ;1,1,5 → 1,5,1
+;
+;Math solution note:
+;1> From right to left, find the first digit(partition number) which violate the
+;increase trend, in this example, 6 will be selected since 8,7,4,3,2 already in
+;a increase trend.
+;
+;2> From right to left, find the first digit which larger than partition number,
+;call it change number. Here the 7 will be selected.
+;
+;3> Swap the partion number and change number.
+;
+;4> Reverse all the digit on the right of partition index.
+;
+;example>
+;init   -  6  8  7  4  3  2
+;step-1 - >6< 8  7  4  3  2
+;step-2 -  6  8 >7< 4  3  2
+;step-3 -  7  8  6  4  3  2
+;step-4 -  7 <8  6  4  3  2>
+;final  -  7  2  3  4  6  8
 
-(define [descending? nums]
+(define [find-first-descending-idx-r nums]
   (let ([len (length nums)])
-    (if [< 1 len]
-      [and [< (second nums) (first nums)] [descending? (rest nums)]]
-      true)))
+    (define [iter idx]
+      (cond ([= 0 idx] -1)
+            ([< (list-ref nums (sub1 idx)) (list-ref nums idx)] (sub1 idx))
+            (else (iter (sub1 idx)))))
+
+    (iter (sub1 len))))
+
+(define [swap-list-elts lst idx-s idx-l]
+  (append (take lst idx-s)
+          (list (list-ref lst idx-l))
+          (take (drop lst (add1 idx-s)) (sub1 (- idx-l idx-s)))
+          (list (list-ref lst idx-s))
+          (drop lst (add1 idx-l))))
 
 (define [next-permutation nums]
-  (let* ([lv (add1 (apply max nums))]
-         [len (length nums)]
-         [pos-ws (map (lambda [p] (expt lv p)) (range (sub1 len) -1 -1))])
-    (define [iter w]
-      (define [available?]
-        (let ([result (map (lambda [n] (remainder (floor (/ w n)) lv)) pos-ws)])
-          [and [andmap = (sort result <) (sort nums <)] result]))
+  (let ([len (length nums)]
+        [sorted-non-duplicated-nums (sort (remove-duplicates nums) <)]
+        [fd-idx (find-first-descending-idx-r nums)])
+    (define [continue]
+      (let* ([fd-num (list-ref nums fd-idx)]
+             [l-num (cadr (memq fd-num sorted-non-duplicated-nums))]
+             [l-idx (ormap (lambda [x] [and [= (list-ref nums x) l-num] x])
+                           (range 0 len))]
+             [swapped-nums (swap-list-elts nums fd-idx l-idx)])
+        (append (take swapped-nums (add1 fd-idx))
+                (reverse (drop swapped-nums(add1 fd-idx))))))
 
-      [or [available?] (iter (add1 w))])
-    
-    (if [descending? nums]
-      (reverse nums)
-      (iter (add1 (apply + (map * nums pos-ws)))))))
+    (if [< fd-idx 0] (reverse nums) (continue))))
 
 (next-permutation '(1 2 3))
 (next-permutation '(3 2 1))
 (next-permutation '(1 1 5))
-;(all-permutations '(1 2 2 3))
+(next-permutation '(1 3 2 2))
