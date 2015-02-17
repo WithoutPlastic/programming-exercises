0bdf264e7154f748a667d6febd18cede6b54d319
PaprikaZ <sms.extreme@gmail.com>
2015-02-17 16:08:08 +0800

    Add leetcode exercise permutation-sequence

---
+++ racket/leetcode/permutation-sequence.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+;Problem:
+;The set [1,2,3,…,n] contains a total of n! unique permutations.
+;
+;By listing and labeling all of the permutations in order,
+;We get the following sequence (ie, for n = 3):
+;
+;"123"
+;"132"
+;"213"
+;"231"
+;"312"
+;"321"
+;
+;Given n and k, return the kth permutation sequence.
+;
+;Note: Given n will be between 1 and 9 inclusive.
+
+(require "permutations-ii.rkt")
+(define [factorial n] (if [< 1 n] (* n (factorial (sub1 n))) 1))
+
+(define [get-permutation n k]
+  (let ([permutations (permute-unique (range 1 (add1 n)))]
+        [permutations-len (factorial n)])
+    (if [<= k permutations-len]
+      (list-ref permutations (sub1 k))
+      (list-ref permutations (sub1 permutations-len)))))
+
+(get-permutation 3 5)
