73f28c23af90ef304896c979e4d871b981e4d4ff
PaprikaZ <sms.extreme@gmail.com>
2015-02-17 15:56:17 +0800

    Update merge-intervals without require insert-intervals

--- racket/leetcode/merge-intervals.rkt
+++ racket/leetcode/merge-intervals.rkt
@@ -7,11 +7,22 @@
 ;Given [1,3],[2,6],[8,10],[15,18],
 ;return [1,6],[8,10],[15,18].
 
-(require "insert-interval.rkt")
-(define [insert-interval new-interval intervals]
-  (insert intervals new-interval))
+(define [merge intervals]
+  (let ([l-sorted (sort intervals (lambda [x y] [< (car x) (car y)]))])
+    (define [iter remaining]
+      (define [continue]
+        (let* ([first-intvl (car remaining)]
+               [fl (car first-intvl)] [fh (cdr first-intvl)]
+               [second-intvl (cadr remaining)]
+               [sl (car second-intvl)] [sh (cdr second-intvl)]
+               [rest-intvls (cdr remaining)])
+          (if [< fh sl]
+            (cons first-intvl (iter rest-intvls))
+            (iter (cons (cons (min fl sl) (max fh sh)) (cdr rest-intvls))))))
 
-(define [merge intervals] (foldl insert-interval '() intervals))
+      (if [or [null? remaining] [null? (cdr remaining)]] remaining (continue)))
+
+    (iter l-sorted)))
 
 (define test-intervals (list '(1 . 3) '(2 . 6) '(8 . 10) '(15 . 18)))
 
