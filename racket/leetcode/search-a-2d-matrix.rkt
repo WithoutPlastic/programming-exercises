17787b5aa1e6d6e4dada3af7354bd6e443b0269b
PaprikaZ <sms.extreme@gmail.com>
2015-02-24 21:19:54 +0800

    Update search-a-2d-matrix with binary search

--- racket/leetcode/search-a-2d-matrix.rkt
+++ racket/leetcode/search-a-2d-matrix.rkt
@@ -21,22 +21,45 @@
 (define [search-matrix matrix target]
   (let ([min-elt (caar matrix)] [max-elt (last (last matrix))])
     (define [iter-col row]
-      (if [null? row] false [or [= target (car row)] (iter-col (cdr row))]))
+      (let ([len (length row)])
+        (define [binary-split]
+          (let* ([mid-idx (floor (/ len 2))]
+                 [mid-elt (list-ref row mid-idx)])
+            (cond ([= mid-elt target] true)
+                  ([< target mid-elt] (iter-col (take row mid-idx)))
+                  (else (iter-col (drop row (add1 mid-idx)))))))
+
+        (cond ([= len 1] [= (car row) target])
+              ([= len 2] [or [= (car row) target] [= (cadr row) target]])
+              (else (binary-split)))))
 
     (define [iter-row matrix]
-      (let ([first-row (car matrix)] [rest-rows (cdr matrix)])
-        (define [continue]
-          (let ([second-row-f-elt (caadr matrix)])
-            (if [< target second-row-f-elt]
-              (iter-col first-row)
-              (iter-row rest-rows))))
+      (let ([len (length matrix)])
+        (define [binary-split]
+          (let* ([mid-idx (floor (/ len 2))]
+                 [mid-row (list-ref matrix mid-idx)]
+                 [mid-row-f-elt (car mid-row)]
+                 [mid-right-idx (add1 mid-idx)]
+                 [mid-right-row (list-ref matrix mid-right-idx)]
+                 [mid-right-f-elt (car mid-right-row)])
+            (cond ([< target mid-row-f-elt] (iter-row (take matrix mid-idx)))
+                  ([< target mid-right-f-elt] (iter-col mid-row))
+                  (else (iter-row (drop matrix mid-right-idx))))))
 
-        (if [null? rest-rows] (iter-col first-row) (continue))))
+        (cond ([= len 1] (iter-col (car matrix)))
+              ([= len 2]
+               (if [< target (caadr matrix)]
+                 (iter-col (car matrix)) (iter-col (cadr matrix))))
+              (else (binary-split)))))
 
     (if [or [null? matrix] [< target min-elt] [< max-elt target]]
       false (iter-row matrix))))
 
-(define test-matrix (list '(1 3 5 7) '(10 11 16 20) '(23 30 34 50)))
+(define test-matrix
+  (list '(1 2 3 4 5 7 8 9)
+        '(10 11 14 15 16 17 19 20)
+        '(23 30 34 41 48 50 55 60)
+        '(80 90 111 120 180 200 212 213)))
 
-(search-matrix test-matrix 3)
-(search-matrix test-matrix 21)
+(search-matrix test-matrix 55)
+(search-matrix test-matrix 210)
