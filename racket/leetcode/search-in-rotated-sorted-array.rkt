5c61808f1d18339574c07c1f560eeb4d1b7c2372
PaprikaZ <sms.extreme@gmail.com>
2015-02-25 00:43:37 +0800

    Clean search-in-rotated-sorted-array

--- racket/leetcode/search-in-rotated-sorted-array.rkt
+++ racket/leetcode/search-in-rotated-sorted-array.rkt
@@ -14,33 +14,32 @@
   (define [iter rs-nums offset]
     (let ([len (length rs-nums)])
       (define [continue]
-        (let* ([ln (first rs-nums)]
-               [rn (last rs-nums)]
+        (let* ([ln (first rs-nums)] [rn (last rs-nums)]
                [mid-idx (floor (/ len 2))]
                [mn (list-ref rs-nums mid-idx)]
                [lp (take (drop rs-nums 1) (sub1 mid-idx))]
-               [rp (take (drop rs-nums (add1 mid-idx)) (- len mid-idx 2))])
+               [rp (drop-right (drop rs-nums (add1 mid-idx)) 1)])
           (cond ([= target ln] offset)
                 ([= target rn] (+ offset (sub1 len)))
                 ([= target mn] (+ offset mid-idx))
-                ([and [< ln mn] [< target mn] [< ln target]]
-                 (iter lp (add1 offset)))
                 ([< ln mn]
-                 (iter rp (+ offset (add1 mid-idx))))
-                ([and [< mn ln] [< mn target] [< target rn]]
+                 (if [and [< target mn] [< ln target]]
+                   (iter lp (add1 offset))
+                   (iter rp (+ offset (add1 mid-idx)))))
+                ([and [< mn target] [< target rn]]
                  (iter rp (+ offset (add1 mid-idx))))
                 (else (iter lp (add1 offset))))))
 
-      (cond ([null? rs-nums] -1)
-            ([< 2 len] (continue))
-            ([= (car rs-nums) target] offset)
-            ([and [pair? (cdr rs-nums)] [= (cadr rs-nums) target]]
-             (add1 offset))
-            (else -1))))
+      (cond ([= len 0] -1)
+            ([and [= len 1] [= (car rs-nums) target]] offset)
+            ([= len 2]
+             (cond ([= (car rs-nums) target] offset)
+                   ([= (cadr rs-nums) target] (add1 offset))
+                   (else -1)))
+            (else (continue)))))
 
   (iter rs-nums 0))
 
 (define test-nums-a '(1 2 3 4 5 6 7 -4 -3 -2 -1 0))
-(define test-target-a 3)
 
 (search test-nums-a 3)
