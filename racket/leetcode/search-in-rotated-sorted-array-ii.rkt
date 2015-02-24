3e8c828e07cd557b8a63ca18396176f5ca7f7f0a
PaprikaZ <sms.extreme@gmail.com>
2015-02-25 00:52:08 +0800

    Update search-in-rotated-sorted-array-ii without remove duplicates

--- racket/leetcode/search-in-rotated-sorted-array-ii.rkt
+++ racket/leetcode/search-in-rotated-sorted-array-ii.rkt
@@ -9,27 +9,28 @@
 ;Write a function to determine if a given target is in the array.
 
 (define [search? rs-nums target]
-  (let ([unique-nums (remove-duplicates rs-nums)])
-    (define [iter remaining]
-      (let ([len (length remaining)])
-        (define [continue]
-          (let* ([ln (first remaining)] [rn (last remaining)]
-                 [mid-idx (floor (/ len 2))]
-                 [mn (list-ref remaining mid-idx)]
-                 [lp (take (drop remaining 1) (sub1 mid-idx))]
-                 [rp (drop-right (drop remaining (add1 mid-idx)) 1)])
-            (cond ([or [= target ln] [= target rn] [= target mn]] true)
-                  ([< ln mn]
-                   (if [and [< target mn] [< ln target]] (iter lp) (iter rp)))
-                  ([and [< mn target] [< target rn]] (iter rp))
-                  (else (iter lp)))))
+  (define [iter remaining]
+    (let ([len (length remaining)])
+      (define [continue]
+        (let* ([ln (first remaining)] [rn (last remaining)]
+               [mid-idx (floor (/ len 2))]
+               [mn (list-ref remaining mid-idx)]
+               [lp (take (drop remaining 1) (sub1 mid-idx))]
+               [rp (drop-right (drop remaining (add1 mid-idx)) 1)])
+          (cond ([or [= target ln] [= target rn] [= target mn]] true)
+                ([< ln mn]
+                 (if [and [< target mn] [< ln target]] (iter lp) (iter rp)))
+                ([< mn rn]
+                 (if [and [< mn target] [< target rn]] (iter rp) (iter lp)))
+                (else (or (iter lp) (iter rp))))))
 
-        (cond ([= len 0] false)
-              ([= len 1] [= (car remaining) target])
-              ([= len 2] [or [= (car remaining) target] [= (cadr remaining) target]])
-              (else (continue)))))
+      (cond ([= len 0] false)
+            ([= len 1] [= (car remaining) target])
+            ([= len 2] [or [= (car remaining) target] [= (cadr remaining) target]])
+            (else (continue)))))
 
-    (iter unique-nums)))
+  (iter rs-nums))
 
 (define test-nums-a '(1 2 3 4 5 6 7 -4 -3 -2 -1 0))
+(search? test-nums-a 3)
 (search? test-nums-a 8)
