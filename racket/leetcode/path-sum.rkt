a4eedbea479a25f2e369e0324c2106d18b652dde
PaprikaZ <sms.extreme@gmail.com>
2015-02-26 16:10:34 +0800

    Add leetcode exercise path-sum

---
+++ racket/leetcode/path-sum.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+;Problem
+;Given a binary tree and a sum, determine if the tree has a root-to-leaf path
+;such that adding up all the values along the path equals the given sum.
+;
+;For example:
+;Given the below binary tree and sum = 22,
+;
+;              5
+;             / \
+;            4   8
+;           /   / \
+;          11  13  4
+;         /  \      \
+;        7    2      1
+;
+;return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+(require "lib/binary-tree.rkt")
+
+(define [has-path-sum? root sum]
+  (define [find-paths node]
+    (let* ([left-br (btree-left node)] [right-br (btree-right node)]
+           [payload (btree-payload node)] [ext-payload (curry cons payload)])
+      (cond ([btree-branches-non-empty? node]
+             (append (map ext-payload (find-paths left-br))
+                     (map ext-payload (find-paths right-br))))
+            ([btree-last? node] (list (list payload)))
+            ([btree-left-empty? node] (map ext-payload (find-paths right-br)))
+            (else (map ext-payload (find-paths left-br))))))
+
+  (if [null? root]
+    false
+    [list? (memq sum (map (curry apply +) (find-paths root)))]))
+
+(define test-tree (btree-parse '(5 4 8 11 - 13 4 7 2 - - - 1)))
+
+(has-path-sum? test-tree 22)
