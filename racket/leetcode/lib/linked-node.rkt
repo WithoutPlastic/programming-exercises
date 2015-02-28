a73022f26c8fa7a8ef9945075656f1bf312c8b33
PaprikaZ <sms.extreme@gmail.com>
2015-02-28 18:44:13 +0800

    Add linked node seek api

--- racket/leetcode/lib/linked-node.rkt
+++ racket/leetcode/lib/linked-node.rkt
@@ -10,6 +10,13 @@
 (define [new-linked-nodes . args]
   (foldr (λ [n pn] (lnode-set-next! n pn) n) '()
          (map (curryr make-lnode '()) args)))
+(define [linked-node-seek head v]
+  (define [walk lnode]
+    (cond ([null? lnode] '())
+          ([equal? (lnode-payload lnode) v] lnode)
+          (else (walk (lnode-next lnode)))))
+
+  (walk head))
 
 (define make-linked-list (curry mcons 'linked-list))
 (define linked-list-head lnode-payload)
@@ -17,5 +24,16 @@
 (define linked-list-set-body! lnode-set-next!)
 (define linked-list-empty? lnode-last?)
 (define new-linked-list (compose make-linked-list new-linked-nodes))
+(define [linked-list-seek linked-list v #:position [pos 'default]]
+  (define [previous-walk lnode]
+    (let ([next-lnode (lnode-next lnode)])
+      (cond ([null? next-lnode] '())
+            ([equal? (lnode-payload next-lnode) v] lnode)
+            (else (previous-walk next-lnode)))))
+
+  (case pos
+    [(previous) (previous-walk linked-list)]
+    [(default) (linked-node-seek (linked-list-body linked-list) v)]
+    [else (error "position keyword incorrect -- LINKED-LIST-SEEK" pos)]))
 
 (provide (all-defined-out))
