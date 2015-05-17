7544f5f89891d30bc912b25744b943a9a39f7c27
PaprikaZ <sms.extreme@gmail.com>
2015-05-17 17:10:44 +0800

    Improve ants solution with O(n)

--- racket/acm-icpc/ch1/ants.rkt
+++ racket/acm-icpc/ch1/ants.rkt
@@ -12,33 +12,13 @@
 ;1 <= n <= 1000000
 ;0 <= xi <= L
 
-(define [permute-direction n]
-  (let iter ([remaining n] [direction-permutation (list '())])
-    (if [<= remaining 0] direction-permutation
-      (iter (sub1 remaining)
-            (append-map (λ [d] (list (cons true d) (cons false d)))
-                        direction-permutation)))))
-
-(define [get-duration stick-length position-list direction-list]
-  (let* ([dir-pos-pair-list (map cons direction-list position-list)]
-         [leftmost-pair-with-right-direction (findf (λ [p] (car p)) dir-pos-pair-list)]
-         [rightmost-pair-with-left-direction (findf (λ [p] [not (car p)])
-                                                    (reverse dir-pos-pair-list))])
-    (max (if [not leftmost-pair-with-right-direction] 0
-           (- stick-length (cdr leftmost-pair-with-right-direction)))
-         (if [not rightmost-pair-with-left-direction] 0
-           (cdr rightmost-pair-with-left-direction)))))
-
-(define [find-all-duration-permutation stick-length position-list]
-  (let* ([n (length position-list)]
-         [direction-permutation (permute-direction n)])
-    (map (curry get-duration stick-length position-list) direction-permutation)))
-
 (define [find-min-duration stick-length position-list]
-  (apply min (find-all-duration-permutation stick-length position-list)))
+  (foldl (λ [pos min-duration] (max min-duration (min pos (- stick-length pos))))
+         0 position-list))
 
 (define [find-max-duration stick-length position-list]
-  (apply max (find-all-duration-permutation stick-length position-list)))
+  (foldl (λ [pos max-duration ] (max max-duration (max pos (- stick-length pos))))
+         0 position-list))
 
 
 (find-min-duration 10 (list 2 6 7))
