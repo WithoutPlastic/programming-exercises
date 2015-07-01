72ae2a1b6731f2d60ed807709fe6709487871ffc
PaprikaZ <sms.extreme@gmail.com>
2015-07-01 22:19:01 +0800

    Add acm-icpc example, 01 bag problem, recursive solution

---
+++ racket/acm-icpc/examples/ch2/01-bag-problem.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+;Problem:
+;There is n goods, each weight Wi, value Vi. Pick up goods with their total weight less than W.
+;Wirte a program to calculate the max value.
+;
+;Restriction:
+;1 <= n <= 100
+;1 <= Wi, Vi <= 100
+;1 <= W <= 10000
+
+
+(define [get-max-value-combination goods-lst max-weight]
+  (let ([weight-lst (map car goods-lst)]
+        [value-lst (map cdr goods-lst)])
+    (cond ([null? goods-lst] 0)
+          ([< max-weight (apply min weight-lst)] 0)
+          ([< max-weight (car weight-lst)] (get-max-value-combination (cdr goods-lst) max-weight))
+          (else (max (get-max-value-combination (cdr goods-lst) max-weight)
+                     (+ (car value-lst)
+                        (get-max-value-combination (cdr goods-lst)
+                                                   (- max-weight (car weight-lst)))))))))
+
+
+(get-max-value-combination (list (cons 2 3) (cons 1 2) (cons 3 4) (cons 2 2)) 5)
